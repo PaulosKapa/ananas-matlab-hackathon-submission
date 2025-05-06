c68e249a77c4a2877e3589da1ff1f03734f5da66
resume12 <52920283+PaulosKapa@users.noreply.github.com>
2025-05-06 14:06:08 +0300

    test

--- ananas-matlab-hackathon-submission/main.m
+++ ananas-matlab-hackathon-submission/main.m
@@ -3,31 +3,24 @@ lon = Position.longitude;
 peaks = Acceleration.Z;
 steps_acc = 0;
 average = mean(peaks);
-earthCirc = 40075 ;
+earthCirc = 6371 ;
 total_distance = 0;
+%calculate gait based on height
+height = input("Height in cm: ");
+gait = height/100*0.414;
+% calculate steps based an accelerometer activity (for reference)
+
 for i = 2:length(peaks)
-    
+
     if(i+1<length(peaks))
+        %check if there is a peak
         if(peaks(i-1)<peaks(i)&&peaks(i+1)<peaks(i)&&peaks(i)>average)
             steps_acc=steps_acc+1;
         end
     end
 
 end
-for i = 1: length(lat)-1
-    
-    lat1 = deg2rad(lat(i));
-    lon1 = deg2rad(lon(i));
-    lat2 = deg2rad(lat(i+1));
-    lon2 = deg2rad(lon(i+1));
-    
-    dlat = lat2 - lat1;
-    dlon = lon2 - lon1;
-    a = sin(dlat/2)^2 + cos(lat1) * cos(lat2) * sin(dlon/2)^2;
-    c = 2 * atan2(sqrt(a), sqrt(1-a));
-    total_distance = total_distance + (earthCirc* c);
-    
-end
+
 steps_acc
-total_distance*1000
-steps = round((total_distance*1000)/0.7)
+total_distance = distance(lat, lon, earthCirc)*1000
+steps = round((total_distance)/gait)
